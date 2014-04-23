4495b5fa0dc16d5a56c733220b2f89e4191f4f4b
fede.dirocco <fede.dirocco@gmail.com>
2014-04-23 00:53:12 +0000

    Agrego clases Perfil y Usuario Un usuario va a contener solo el nombre y la contraseña mientras que un perfil va a contener sus datos personales, foto y hasta podria tener un inbox/chat o algo para comunicarse con los demas

---
+++ cinemaweb/grails-app/domain/cinemaweb/Perfil.groovy
@@ -0,0 +1,31 @@
+package cinemaweb
+
+class Perfil {
+
+	String nombre
+	String apellido
+	String email
+	byte[] foto
+	String localidad
+	String pais
+	//Inbox inbox
+
+	//static hasMany = [ circulos:Circulo, promociones:Promocion ]
+
+	static belongsTo = Usuario
+
+    static constraints = {
+
+    	nombre blank:false
+
+    	apellido blank:false
+    	
+    	email email:true, blank:false, unique:true
+
+    	foto nullable:true
+
+    	localidad blank:true
+
+    	pais blank:true
+    }
+}
