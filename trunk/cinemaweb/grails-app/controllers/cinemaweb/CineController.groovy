5134d04a8eca64b0f771b6debf7815f8fa246ea0
matias.barro@gmail.com <matias.barro@gmail.com@59e9e142-6434-eb1c-4cfd-685dc8ab8e6a>
2014-06-05 00:06:51 +0000

    abm de cine terminado

--- trunk/cinemaweb/grails-app/controllers/cinemaweb/CineController.groovy
+++ trunk/cinemaweb/grails-app/controllers/cinemaweb/CineController.groovy
@@ -3,11 +3,57 @@ package cinemaweb
 class CineController {
 	static scaffold = true
 
+    def create = {
+
+        def data = [nombre: "",ubicacion: "",precioBase: "20", apertura: "08:00", cierre:"00:00"]
+        def cine = null
+        if(params.submit){
+            data = params
+            cine = new Cine(nombre: params.nombre, ubicacion: params.ubicacion, precioBase: params.float("precioBase"),apertura: params.apertura, cierre: params.cierre, foto: "default")
+            if(cine.validate()){
+                cine.save();
+                redirect(action:"show", id: cine.id)
+            }        
+        }
+
+        def model = [data: data, hours: this.getOpenCloseHours(), cine: cine, action: "create", id: "",back: [action:"index",id:""]]
+        render(view: "create",model: model)
+    }
+
+
+    def edit = {
+        def cine = Cine.get(params.id)
+        def data = cine
+        if (params.submit) {
+            cine.nombre = params.nombre
+            cine.ubicacion = params.ubicacion
+            cine.precioBase = params.float("precioBase")
+            cine.apertura = params.apertura
+            cine.cierre = params.cierre
+            if (cine.validate()) {
+                cine.save();
+                redirect(action:"show", id: cine.id)
+            }
+        }
+
+        def model = [data: data, hours: this.getOpenCloseHours(), cine: cine, action: "edit", id: cine.id,back: [action:"show",id:cine.id]]
+        render(view: "create",model: model)
+    }
+
+    
     def show = {
     	def cine = Cine.get(params.id)
-    	def comentarios = cine.obtenerComentarios()
+    	//def comentarios = cine.obtenerComentarios()
 
-    	[cine: cine, comentarios: comentarios]
+    	//[cine: cine, comentarios: comentarios]
+        [cine: cine]
+    }
+
+
+    def index = {
+        def cines = Cine.list()
+
+        [cines: cines]
     }
 
     def comentar = {
@@ -16,5 +62,15 @@ class CineController {
 
     	usuario.comentar(cine, params.mensaje)
     	redirect(action: "show", id:params.id)
+    }
+
+
+    //metodos auxiliares
+
+    //devuelve el listado de horas de apertura y cierre para el pulldown
+    private def getOpenCloseHours(){
+        return ["00:00", "01:00","02:00","03:00","04:00","05:00","06:00","07:00","08:00","09:00","10:00","11:00",
+                "12:00", "13:00","14:00", "15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00"
+        ]
     }
 }
---
+++ trunk/cinemaweb/grails-app/views/cine/create.gsp
@@ -0,0 +1,65 @@
+<html>
+<head>
+	<meta name="layout" content="bootstrap-main"/>
+	<title>Cinema Web - Nuevo Cine</title>
+</head>
+<body>
+	<div class="container">
+		<g:if test="${cine && cine.hasErrors()}">
+			<div class="row">
+				<div class="col-md-8">
+					<div id="message_error">
+						<g:renderErrors bean="${cine}" as="list" />
+					</div>
+				</div>
+			</div>
+		</g:if>
+		<div class="row">
+			<div class="col-md-8">
+				<h4>Nuevo Cine</h4>
+				<g:form action="${action}" id="${id}" class="form-horizontal">
+				<div class="form-group">
+					<label for="name" class="col-md-2 col-sm-2 control-label">Nombre:</label>
+					<div class="col-md-4">
+						<input name="nombre" class="form-control" id="name" value="${data.nombre}">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="ubicacion" class="col-md-2 col-sm-2 control-label">Ubicacion:</label>
+					<div class="col-md-4">
+						<input name="ubicacion" class="form-control" id="ubicacion" value="${data.ubicacion}">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="precio" class="col-md-2 col-sm-2 control-label">Precio Base:</label>
+					<div class="col-md-2">
+						<input name="precioBase" class="form-control" id="precio" value="${data.precioBase}">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="apertura" class="col-md-2 col-sm-2 control-label">Apertura:</label>
+					<div class="col-md-2">
+						<g:select name="apertura" class="form-control" from="${hours}" value="${data.apertura}" />
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="cierre" class="col-md-2 col-sm-2 control-label">Cierre:</label>
+					<div class="col-md-2">
+						<g:select name="cierre" class="form-control" from="${hours}" value="${data.cierre}" />
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-sm-10 col-md-2 col-md-offset-2">
+						<input type="hidden" name="submit" value="1">
+						<g:link controller="cine" action="${back.action}" id="${back.id}" class="btn btn-default btn-block">Volver</g:link>
+					</div>
+					<div class="col-sm-10 col-md-2">
+						<button type="submit" class="btn btn-primary btn-block">Crear</button>
+					</div>
+				</div>
+			</g:form>
+		</div>	
+	</div>	
+</div>	
+</body>
+</html>
