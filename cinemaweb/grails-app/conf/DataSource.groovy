8c4df41ae17de10fa881f080eac1deed8228d42d
matias.barro <matias.barro@gmail.com>
2014-06-14 19:39:38 +0000

    configuracion para mysql

--- cinemaweb/grails-app/conf/DataSource.groovy
+++ cinemaweb/grails-app/conf/DataSource.groovy
@@ -1,9 +1,11 @@
 dataSource {
     pooled = true
-    jmxExport = true
+    /*jmxExport = true
     driverClassName = "org.h2.Driver"
     username = "sa"
-    password = ""
+    password = ""*/
+    driverClassName = "com.mysql.jdbc.Driver"
+    dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -17,8 +19,11 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+           /* url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"*/
+            url = "jdbc:mysql://localhost/cinemaweb?useUnicode=yes&characterEncoding=UTF-8"
+            username = "root"
+            password = "12345"
         }
     }
     test {
