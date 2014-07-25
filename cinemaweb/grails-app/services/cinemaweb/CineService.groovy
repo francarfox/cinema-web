60632bb6b28b5d0e5091383f8ff3008b79aeedd6
matias.barro <matias.barro@gmail.com>
2014-07-25 22:19:07 +0000

    vuelvo a crear servicio de cines

---
+++ cinemaweb/grails-app/services/cinemaweb/CineService.groovy
@@ -0,0 +1,22 @@
+package cinemaweb
+
+import grails.transaction.Transactional
+
+class CineService extends DomainService{
+
+    @Transactional
+	public def getDomainInstance(String id=""){
+		return (id) ? Cine.get(id) : new Cine()
+	}
+
+	@Transactional
+	public def getCine(String id){
+		return this.getDomainInstance(id)
+	}
+
+	@Transactional
+	public def getListadoCines() {
+		return Cine.list()
+	}
+
+}
