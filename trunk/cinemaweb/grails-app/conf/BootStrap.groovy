b5f7f735eececf4d9f951a5496158bf1e6d01fe8
fede.dirocco@gmail.com <fede.dirocco@gmail.com@59e9e142-6434-eb1c-4cfd-685dc8ab8e6a>
2014-08-01 21:54:35 +0000

    Version 3 del home. Mejore el diseño, agregue fotos para casi todo y un par mas de peliculas. Me faltaria agregar los ultimos comentarios y nose si algo mas en el home, ya casi esta terminado.
    Dsp cuando pueda voy a hacer la pagina de contacto y refactorizar mi codigo mas algun detalle mas que falte.

--- trunk/cinemaweb/grails-app/conf/BootStrap.groovy
+++ trunk/cinemaweb/grails-app/conf/BootStrap.groovy
@@ -21,16 +21,17 @@ class BootStrap {
 		}
 
 		if(!Cine.count()) {
-		    new Cine(nombre: "Hoyts Unicenter",ubicacion: "Parana 1234, Martinez", precioBase: 12.0, apertura: "08:00", cierre: "23:00", foto:"default.png").save(failOnError:true)
-		    new Cine(nombre: "Showcase Norte",ubicacion: "Esteban Echeberría 456, Martinez", precioBase: 65, apertura: "08:00", cierre: "23:00", foto:"default.png").save(failOnError:true)
-		    new Cine(nombre: "Hoyts Abasto",ubicacion: "Av. Corrientes 3200, Almagro", precioBase: 65, apertura: "08:00", cierre: "23:00", foto:"default.png").save(failOnError:true)
-		    new Cine(nombre: "Village Caballito",ubicacion: "Av. Rivadavia 5071, Caballito", precioBase: 65, apertura: "08:00", cierre: "23:00", foto:"default.png").save(failOnError:true)
-		    new Cine(nombre: "Belgrano Multiplex",ubicacion: "Vuelta de Obligado 2237, Belgrano", precioBase: 65, apertura: "08:00", cierre: "23:00", foto:"default.png").save(failOnError:true)
-		    new Cine(nombre: "Atlas Patio Bullrich",ubicacion: "Av. del Libertador 750,Recoleta", precioBase: 65, apertura: "08:00", cierre: "23:00", foto:"default.png").save(failOnError:true)
-		    new Cine(nombre: "Showcase Belgrano",ubicacion: "Monroe 1655, Belgrano", precioBase: 65, apertura: "08:00", cierre: "23:00", foto:"default.png").save(failOnError:true)
-		    new Cine(nombre: "Cultural San Martín",ubicacion: "Sarmiento 1551, Monserrat", precioBase: 65, apertura: "08:00", cierre: "23:00", foto:"default.png").save(failOnError:true)
-		    new Cine(nombre: "Arte Cinema",ubicacion: "Salta 1620, CABA", precioBase: 65, apertura: "08:00", cierre: "23:00", foto:"default.png").save(failOnError:true)
-		    new Cine(nombre: "Cinemark Palermo",ubicacion: "Beruti 3399, Palermo", precioBase: 65, apertura: "08:00", cierre: "23:00", foto:"default.png").save(failOnError:true)
+		    new Cine(nombre: "Hoyts Unicenter",ubicacion: "Parana 1234, Martinez", precioBase: 12.0, apertura: "08:00", cierre: "23:00", foto:"hoyts.png").save(failOnError:true)
+		    new Cine(nombre: "Showcase Norte",ubicacion: "Esteban Echeberría 456, Martinez", precioBase: 65, apertura: "08:00", cierre: "23:00", foto:"showcase..png").save(failOnError:true)
+		    new Cine(nombre: "Hoyts Abasto",ubicacion: "Av. Corrientes 3200, Abasto", precioBase: 65, apertura: "08:00", cierre: "23:00", foto:"hoyts.png").save(failOnError:true)
+		    new Cine(nombre: "Village Caballito",ubicacion: "Av. Rivadavia 5071, Caballito", precioBase: 65, apertura: "08:00", cierre: "23:00", foto:"village.png").save(failOnError:true)
+		    new Cine(nombre: "Belgrano Multiplex",ubicacion: "Vuelta de Obligado 2237, Belgrano", precioBase: 65, apertura: "08:00", cierre: "23:00", foto:"multiplex.png").save(failOnError:true)
+		    new Cine(nombre: "Atlas Patio Bullrich",ubicacion: "Av. del Libertador 750,Recoleta", precioBase: 65, apertura: "08:00", cierre: "23:00", foto:"atlas.png").save(failOnError:true)
+		    new Cine(nombre: "Showcase Belgrano",ubicacion: "Monroe 1655, Belgrano", precioBase: 65, apertura: "08:00", cierre: "23:00", foto:"showcase.png").save(failOnError:true)
+		    new Cine(nombre: "Cultural San Martín",ubicacion: "Sarmiento 1551, Monserrat", precioBase: 65, apertura: "08:00", cierre: "23:00", foto:"centroculturalsanmartin.png").save(failOnError:true)
+		    //new Cine(nombre: "Arte Cinema",ubicacion: "Salta 1620, CABA", precioBase: 65, apertura: "08:00", cierre: "23:00", foto:"default.png").save(failOnError:true)
+		    new Cine(nombre: "Cinemark Palermo",ubicacion: "Beruti 3399, Palermo", precioBase: 65, apertura: "08:00", cierre: "23:00", foto:"cinemark.png").save(failOnError:true)
+			new Cine(nombre: "Arteplex Belgrano",ubicacion: "Av. Cabildo 2829, Belgrano", precioBase: 65, apertura: "08:00", cierre: "23:00", foto:"arteplex.png").save(failOnError:true)
 		}
 
 		if(!Pelicula.count()) {
@@ -39,25 +40,28 @@ class BootStrap {
 		    new Pelicula(titulo: "Socios por accidente", director: "Fabián Forte, Nicanor Loreti", sinopsis: "Comedia", duracion: 82, foto:"socios-por-accidente.jpg", urlTrailer:"asd").save(failOnError:true)
 		    new Pelicula(titulo: "El planeta de los simios: Confrontación", director: "Matt Reeves", sinopsis: "Aventuras", duracion: 130, foto:"el-planeta-de-los-simios-confrontacion.jpg", urlTrailer:"asd").save(failOnError:true)
 		    new Pelicula(titulo: "12 horas para sobrevivir", director: "James DeMonaco", sinopsis: "Suspenso", duracion: 103, foto:"12-hrs-para-sobrevivir.jpg", urlTrailer:"asd").save(failOnError:true)
+		    new Pelicula(titulo: "Transformers 4", director: "Michael Bay", sinopsis: "Acción", duracion: 165, foto:"transformers-4.jpg", urlTrailer:"asd").save(failOnError:true)
 		    new Pelicula(titulo: "Guardianes de la Galaxia", director: "James Gunn", sinopsis: "Ciencia Ficción", duracion: 120, foto:"guardianes-de-la-galaxia.jpg", urlTrailer:"asd").save(failOnError:true)
-		    
+		    new Pelicula(titulo: "Bajo la misma estrella", director: "Josh Boone", sinopsis: "Drama", duracion: 126, foto:"bajo-la-misma-estrella.jpg", urlTrailer:"asd").save(failOnError:true)
+		    new Pelicula(titulo: "Aviones 2: Equipo de rescate", director: "Roberts Gannaway", sinopsis: "Animación", duracion: 83, foto:"aviones-2-equipo-de-rescate.jpg", urlTrailer:"asd").save(failOnError:true)
+
 		}
 
 		if(!Circulo.count()) {
 			def user = Usuario.get(4)//mati09
-			def circ = new Circulo(nombre: "StarWars fanaticos", tags: "Acción", administrador: user.getUserId(), foto: "default.png").save(failOnError:true)
+			def circ = new Circulo(nombre: "StarWars fanaticos", tags: "Acción", administrador: user.getUserId(), foto: "circulo-starwars.jpg").save(failOnError:true)
 			user.addToCirculos(circ)
 
 			user = Usuario.get(2) //Rosa69
-			circ = new Circulo(nombre: "Solo terror", tags: "Terror", administrador: user.getUserId(), foto: "default.png").save(failOnError:true)
+			circ = new Circulo(nombre: "Solo terror", tags: "Terror", administrador: user.getUserId(), foto: "circulo-terror.jpg").save(failOnError:true)
 			user.addToCirculos(circ)
 
 			user = Usuario.get(1) //fedecarp34
-			circ = new Circulo(nombre: "Juntada por viejas pelis", tags: "Suspenso", administrador: user.getUserId(), foto: "default.png").save(failOnError:true)
+			circ = new Circulo(nombre: "Juntada por viejas pelis", tags: "Suspenso", administrador: user.getUserId(), foto: "circulo-pelisviejas.jpg").save(failOnError:true)
 			user.addToCirculos(circ)
 
 			user = Usuario.get(3) //FranCarFox
-			circ = new Circulo(nombre: "Las mejores peliculas cómicas ", tags: "Cómica", administrador: user.getUserId(), foto: "default.png").save(failOnError:true)
+			circ = new Circulo(nombre: "Las mejores peliculas cómicas ", tags: "Cómica", administrador: user.getUserId(), foto: "circulo-risa.jpg").save(failOnError:true)
 			user.addToCirculos(circ)
 		}
 
--- trunk/cinemaweb/grails-app/controllers/cinemaweb/HomeController.groovy
+++ trunk/cinemaweb/grails-app/controllers/cinemaweb/HomeController.groovy
@@ -4,12 +4,22 @@ class HomeController {
 
     def index() { 
     	def usuarios = Usuario.list(max: 4,  sort: "id", order: "desc")
-    	def circulos = Circulo.list(max: 4,  sort: "id", order: "desc")
+    	def circulos = Circulo.list(max: 4,  sort: "id", order: "desc") //ordenar por circulo.usuarios.size()
     	def circulosUlt = Circulo.list(max: 3,  sort: "id", order: "desc")
-    	def cines = Cine.list(max: 7,  sort: "id", order: "desc")
-    	def peliculas = Pelicula.list(max: 4,  sort: "id", order: "desc")
+    	def cines = Cine.list(max: 7,  sort: "id", order: "desc") //ordenar por cine.comentarios.size()
+    	def peliculasPunt = Pelicula.list(max: 4,  sort: "puntos", order: "desc")
+    	def peliculas1 = Pelicula.list(max: 7,  sort: "id", order: "desc")
+    	def peliculas2 = Pelicula.list(max: 7,  offset: 7, sort: "id", order: "desc")
 
 
-    	[usuarios:usuarios,circulos:circulos,circulosUlt:circulosUlt,cines:cines,peliculas:peliculas]
+    	[
+    		usuarios:usuarios,
+    		circulos:circulos,
+    		circulosUlt:circulosUlt,
+    		cines:cines,
+    		peliculasPunt:peliculasPunt,
+    		peliculas1:peliculas1,
+    		peliculas2:peliculas2
+    	]
     }
 }
