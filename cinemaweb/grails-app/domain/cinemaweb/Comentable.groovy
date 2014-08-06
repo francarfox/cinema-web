ea5ffbd05b48e153258131fa7b76180119f2c5e8
fede.dirocco <fede.dirocco@gmail.com>
2014-08-06 00:34:45 +0000

    Agrego la clase abstracta Comentable que no me la habia subido. Mati fijate que me esta tirando quilombo con Administrador.groovy / AdministradorController.groovy y administrador.gsp si las tenes en tu repo borralas.

---
+++ cinemaweb/grails-app/domain/cinemaweb/Comentable.groovy
@@ -0,0 +1,30 @@
+package cinemaweb
+
+abstract class Comentable {
+
+	Set<Comentario> comentarios = []
+    Boolean esComentableEnHome = true
+    Integer cantComentarios = 0
+
+	static hasMany = [comentarios: Comentario]
+
+    static constraints = {
+    	
+    }
+
+    def agregarComentario(comentario) {
+    	comentarios.add(comentario)
+    }
+
+    def obtenerComentarios() {
+        return this.comentarios.sort{a, b -> a.id <=> b.id }.reverse()
+    }
+
+    def getCantidadComentarios() {
+        return this.comentarios.size()
+    }
+
+    def sumarCantidadComentarios() {
+        this.cantComentarios = this.cantComentarios + 1
+    }
+}
